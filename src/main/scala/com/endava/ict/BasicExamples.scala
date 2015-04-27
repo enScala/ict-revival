13315475c82e49dc85a7ae45174b5d2d66eed8f5
Dorin Cobzac <dorin.cobzac@endava.com>
2015-04-27 21:33:18 +0300

    add basic examples (functions as values, closure)
    Change-Id: I12a804567bd74a94ab1ab905e890d2ec4f75e0fa

---
+++ src/main/scala/com/endava/ict/BasicExamples.scala
@@ -0,0 +1,45 @@
+package com.endava.ict
+
+object FunctionsAsValues {
+
+  val one : Int = 1
+  val two : Int = 2
+
+  /**
+   * Function takes two Ints as arguments and returns their sum as a String value
+   *
+   * The function itself is no different than any other value (i.e. it can be passed as an argument to another function)
+   */
+  val func : (Int, Int) => String = (a: Int, b: Int) =>
+  {
+    (a + b).toString
+  }
+
+  /**
+   * Function that takes another function as an argument
+   */
+  def sumOfTwoIntegers(f: (Int, Int) => String) : String =
+  {
+    /**
+     * apply the function value passed as the argument
+     */
+    f(1, 2)
+  }
+}
+
+object Closure {
+
+  var one = 2
+
+  /**
+   * Function is considered *open* since it needs the variable *one*, which is defined outside its scope
+   * At runtime it captures (also called it *closes* over) *one*, hence the name *closure*
+   */
+  def func(a : Int) : Int = {
+    a + one
+  }
+}
+
+
+
+
