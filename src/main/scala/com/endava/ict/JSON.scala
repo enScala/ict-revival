89c0c4d3c27d8b013c1bd3a6fb4f6836e1641b70
driekken <driekken@gmail.com>
2015-04-27 20:21:02 +0300

    add functional data structures examples

---
+++ src/main/scala/com/endava/ict/JSON.scala
@@ -0,0 +1,9 @@
+package com.endava.ict
+
+sealed trait JSON
+case object JNull extends JSON
+case class JNumber(num: Double) extends JSON
+case class JString(str: String) extends JSON
+case class JBoolean(b: Boolean) extends JSON
+case class JArray(elems: List[JSON]) extends JSON
+case class JObject(bindings: Map[String, JSON]) extends JSON
---
+++ src/main/scala/com/endava/ict/Tree.scala
@@ -0,0 +1,57 @@
+package com.endava.ict
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  def double(t: Tree[Int]): Tree[Int] =
+    t match {
+      case Leaf(v) => Leaf(v * 2)
+      case Branch(left, right) => Branch(double(left), double(right))
+    }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    t match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+    }
+
+  def double2(t: Tree[Int]): Tree[Int] =
+    map(t)((elem: Int) => 2 * elem)
+
+  def sum(t: Tree[Int]): Int =
+    t match {
+      case Leaf(v) => v
+      case Branch(left, right) => sum(left) + sum(right)
+    }
+
+  def product(t: Tree[Int]): Int =
+    t match {
+      case Leaf(v) => v
+      case Branch(left, right) => product(left) * product(right)
+    }
+
+  def size[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_) => 1
+      case Branch(left, right) => size(left) + size(right)
+    }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B =
+    t match {
+      case Leaf(v) => f(v)
+      case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+    }
+
+  def sum2(t: Tree[Int]): Int =
+    fold(t)((elem: Int) => elem)((l, r) => l + r)
+
+  def product2(t: Tree[Int]): Int =
+    fold(t)((elem: Int) => elem)((l, r) => l * r)
+
+  def size2(t: Tree[Int]): Int =
+    fold(t)((elem: Int) => 1)((l, r) => l + r)
+
+}
